16de219db0b5a67bf9556e39a31c16e36d6e138c
ty <tylerpmatthews1998@gmail.com>
2021-02-05 18:03:48 -0500

    MaterialUI to the register form

--- watermyplants/src/Components/Register.js
+++ watermyplants/src/Components/Register.js
@@ -1,48 +1,39 @@
 import React, { useEffect, useState } from "react";
-import { Link, useHistory } from "react-router-dom";
+import { useHistory } from "react-router-dom";
 import * as yup from "yup";
-import styled from "styled-components";
 import axios from "axios";
 import "./style.css";
-import { REGISTER_URL } from '../Util/Private'
+import { REGISTER_URL } from "../Util/Private";
+import Avatar from "@material-ui/core/Avatar";
+import Button from "@material-ui/core/Button";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import TextField from "@material-ui/core/TextField";
+import Link from "@material-ui/core/Link";
+import Grid from "@material-ui/core/Grid";
+import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/core/styles";
+import Container from "@material-ui/core/Container";
 
-// Styled-Components
-const StyledRegisterContainer = styled.div`
-  color: black;
-  height: auto;
-  width: auto;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  text-align: center;
-  margin: 1px 1px;
-  flex-wrap: wrap;
-  flex-direction: column;
-`;
-
-const StyledForm = styled.form`
-  height: auto;
-`;
-
-const StyledInputs = styled.div`
-  color: black;
-  height: auto;
-  width: auto;
-  display: flex;
-  border: 3px solid slategray;
-  box-shadow: 0.8rem 0.8rem gray;
-  align-items: center;
-  justify-content: space-evenly;
-  text-align: match-parent;
-  padding: 5% 5% 5% 5%;
-  margin: 0% 0% 0% 0%;
-  flex-wrap: wrap;
-  flex-direction: column;
-`;
-
-const StyledLoginLink = styled.div`
-  margin: 10% auto auto auto;
-`;
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    marginTop: theme.spacing(8),
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: "100%", // Fix IE 11 issue.
+    marginTop: theme.spacing(3),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+}));
 
 const schema = yup.object().shape({
   username: yup
@@ -64,12 +55,8 @@ const initialFormValues = {
 
 const Register = () => {
   const [value, setValue] = useState(initialFormValues);
-  const [disabled, setDisabled] = useState(true);
   const { push } = useHistory();
-
-  useEffect(() => {
-    schema.isValid(value).then((valid) => setDisabled(!valid));
-  }, [value]);
+  const classes = useStyles();
 
   const onChange = (e) => {
     setValue({
@@ -85,71 +72,83 @@ const Register = () => {
       .then((res) => {
         console.log(res);
         window.alert("User account creation: Successful!");
-        push("/");
+        push("/plantlist");
       })
       .catch((err) => {
         console.log("Login Axios error", err.response);
       });
   };
 
-  const pageChangeReset = () => {
-    setValue(initialFormValues);
-  };
-
   return (
-    <StyledRegisterContainer>
-      <h1> Welcome to Water My Plants Registration!</h1>
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <div className={classes.paper}>
+        <Avatar className={classes.avatar}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          Sign up
+        </Typography>
+        <form className={classes.form} noValidate onSubmit={onSubmit}>
+          <Grid container spacing={2}>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                name="username"
+                variant="outlined"
+                required
+                fullWidth
+                id="username"
+                label="username"
+                autoFocus
+                onChange={onChange}
+              />
+            </Grid>
 
-      <StyledForm onSubmit={onSubmit}>
-        <StyledInputs>
-          <label>
-            Username
-            <input
-              name="username"
-              type="text"
-              onChange={onChange}
-              value={value.username}
-              placeholder="Username"
-            />
-          </label>
-          <label>
-            Email
-            <input
-              name="email"
-              type="email"
-              onChange={onChange}
-              value={value.email}
-              placeholder="Email"
-            />
-          </label>
-          <label>
-            Password
-            <input
-              name="password"
-              type="password"
-              onChange={onChange}
-              value={value.password}
-              placeholder="Password"
-            />
-          </label>
-          <button disabled={disabled} className="submit-btn">
-            Register
-          </button>
-        </StyledInputs>
-        <div className="errors">
-          {/* <div>{errors.username}</div>
-                <div>{errors.password}</div>
-                <div>{errors.phoneNumber}</div> */}
-        </div>
-        <StyledLoginLink>
-          Already Have An Account?
-          <Link to="/" onClick={pageChangeReset}>
-            <br></br>
-            Login
-          </Link>
-        </StyledLoginLink>
-      </StyledForm>
-    </StyledRegisterContainer>
+            <Grid item xs={12}>
+              <TextField
+                variant="outlined"
+                required
+                fullWidth
+                id="email"
+                label="Email Address"
+                name="email"
+                onChange={onChange}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                variant="outlined"
+                required
+                fullWidth
+                name="password"
+                label="Password"
+                type="password"
+                id="password"
+                autoComplete="current-password"
+                onChange={onChange}
+              />
+            </Grid>
+          </Grid>
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            className={classes.submit}
+          >
+            Sign Up
+          </Button>
+          <Grid container justify="flex-end">
+            <Grid item>
+              <Link href="" variant="body2">
+                Already have an account? Sign in
+              </Link>
+            </Grid>
+          </Grid>
+        </form>
+      </div>
+    </Container>
   );
 };
+
 export default Register;
